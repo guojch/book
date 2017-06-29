6a5443461262a03aeadff437eca173d6662d8d10
guojch <378276308@qq.com>
2017-06-30 00:37:43 +0800

    版本号管理，统一资源管理方法。

--- assets/WapAsset.php
+++ assets/WapAsset.php
@@ -17,15 +17,29 @@ class WapAsset extends AssetBundle
 {
     public $basePath = '@webroot';
     public $baseUrl = '@web';
-    public $css = [
-        'font-awesome/css/font-awesome.css',
-        'css/m/css_style.css',
-        'css/m/app.css?ver=20170401',
-    ];
-    public $js = [
-    ];
-//    public $depends = [
-//        'yii\web\YiiAsset',
-//        'yii\bootstrap\BootstrapAsset',
+//    public $css = [
+//        'font-awesome/css/font-awesome.css',
+//        'css/m/css_style.css',
+//        'css/m/app.css?ver=20170401',
 //    ];
+//    public $js = [
+//    ];
+    public function registerAssetFiles($view) {
+        
+        $release_version = defined('RELEASE_VERSION')?RELEASE_VERSION:time();
+        
+        $this->css = [
+            'css/web/bootstrap.min.css',
+            'font-awesome/css/font-awesome.css',
+            'css/web/style.css?ver='.$release_version,
+        ];
+        
+        $this->js = [
+            'plugins/jquery-2.1.1.js',
+            'js/wap/TouchSlide.1.1.js',
+            'js/wap/common.js?ver='.$release_version,
+        ];
+        
+        parent::registerAssetFiles($view);
+    }
 }
--- assets/WebAsset.php
+++ assets/WebAsset.php
@@ -17,18 +17,36 @@ class WebAsset extends AssetBundle
 {
     public $basePath = '@webroot';
     public $baseUrl = '@web';
-    public $css = [
-        'css/web/bootstrap.min.css',
-        'font-awesome/css/font-awesome.css',
-        'css/web/style.css?ver=20170401',
-    ];
-    public $js = [
-        'plugins/jquery-2.1.1.js',
-        'js/web/bootstrap.min.js',
-        'js/web/common.js',
-    ];
-//    public $depends = [
-//        'yii\web\YiiAsset',
-//        'yii\bootstrap\BootstrapAsset',
+//    public $css = [
+//        'css/web/bootstrap.min.css',
+//        'font-awesome/css/font-awesome.css',
+//        'css/web/style.css?ver='.RELEASE_VERSION,
 //    ];
+//    public $js = [
+//        'plugins/jquery-2.1.1.js',
+//        'js/web/bootstrap.min.js',
+//        'js/web/common.js?ver='.RELEASE_VERSION,
+//    ];
+
+    //上面这种是yii官方默认提供的统一资源管理方法，
+    //实际上，使用下列这种方法更好，
+    //应用场景：版本号是一个常量，加载js或者css是有业务逻辑的。
+    public function registerAssetFiles($view) {
+        
+        $release_version = defined('RELEASE_VERSION')?RELEASE_VERSION:time();
+        
+        $this->css = [
+            'css/web/bootstrap.min.css',
+            'font-awesome/css/font-awesome.css',
+            'css/web/style.css?ver='.$release_version,
+        ];
+        
+        $this->js = [
+            'plugins/jquery-2.1.1.js',
+            'js/web/bootstrap.min.js',
+            'js/web/common.js?ver='.$release_version,
+        ];
+        
+        parent::registerAssetFiles($view);
+    }
 }
